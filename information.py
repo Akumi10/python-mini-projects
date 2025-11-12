f81e64e45874af71b17fe49e3b3b49f2eb40d69c
Akumi10 <inspghali2022@gmail.com>
2025-11-12 20:09:42 +0100

    information

---
+++ information.py
@@ -0,0 +1,12 @@
+info = {
+    "name":"Akumi",
+    "age": 16,
+    "grade":18
+}
+print(info["name"])
+info.update({"city":"boston"})
+school = info.get({"school":"unknow"})
+print(school)
+
+
+print(info)
